c8360b2ed33246570198617477a37b86d7dd00f1
Juan Mallet <El-Juank@users.noreply.github.com>
2020-09-29 19:49:03 +0200

    Modificacions generals
    He arreclat un parell de cosetes d'aquí i d'allà i fet modificacions en general.

--- web_services/node.js
+++ web_services/node.js
@@ -1,70 +1,153 @@
-var express = require('express') //connectar la llibreria Express i es guarda en la variable espress. Es una llibreria obligatoria, sempre es necessita
-var app = express() //Variable principal a la que se li crea una instancia de la llibreria express. Respresenta el servidor
-var mysql = require('mysql')
-var cors = require('cors')
+var express = require("express"); //connectar la llibreria Express i es guarda en la variable espress. Es una llibreria obligatoria, sempre es necessita
+var app = express(); //Variable principal a la que se li crea una instancia de la llibreria express. Respresenta el servidor
+var mysql = require("mysql");
+var cors = require("cors");
 app.use(cors());
-app.get("/", (req, res) => {//s'han de definir una o varies rutes, req = request per recollir les dades, res = resultat de la crida, dades que ha recollit
-    //operativa i resposta, per exemple connectar una base de dades i fer un select per recollir les dades
-    res.send("MyServer") //Aqui es connectara amb una base de dades en JSON
+app.get("/", (req, res) => {
+  //s'han de definir una o varies rutes, req = request per recollir les dades, res = resultat de la crida, dades que ha recollit
+  //operativa i resposta, per exemple connectar una base de dades i fer un select per recollir les dades
+  res.send("El web service està operatiu!"); //Aqui es connectara amb una base de dades en JSON
 });
+//WS Per afegir usuaris
 app.get("/insert/user", (req, res) => {
-    var mysql = require('mysql');
+  var mysql = require("mysql");
 
-    var con = mysql.createConnection({
-        host: "217.61.130.155",
-        user: "admin_games",
-        password: "12345678",
-        database: "admin_games"
-    });
+  var con = mysql.createConnection({
+    host: "217.61.130.155",
+    user: "admin_games",
+    password: "12345678",
+    database: "admin_games",
+  });
 
-    con.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO usuari (usuarinom, usuaricorreu, usuarifoto) VALUES ('"+req.query.nombre+"', '"+req.query.correo+"', '"+req.query.foto+"')";
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("1 record inserted");
-            res.header("Content-Type", "application/json");
-            res.json({});
-            con.end();//Tancar la connexió, és molt important tancar-la sempre
-        });
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql =
+      "INSERT INTO usuari (usuarinom, usuaricorreu, usuarifoto) VALUES ('" +
+      req.query.nombre +
+      "', '" +
+      req.query.correo +
+      "', '" +
+      req.query.foto +
+      "')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+      res.header("Content-Type", "application/json");
+      res.json({});
+      con.end(); //Tancar la connexió, és molt important tancar-la sempre
     });
+  });
 });
-
-/**------------------------------------------------------------------------------------------------------- */
+/*-------------------------------------------------------------------------------------------------------*/
+//WS per iniciar sessió
 app.get("/get/login", (req, res) => {
-    var mysql = require('mysql');
+  var mysql = require("mysql");
 
-    var con = mysql.createConnection({
-        host: "217.61.130.155",
-        user: "admin_games",
-        password: "12345678",
-        database: "admin_games"
-    });
+  var con = mysql.createConnection({
+    host: "217.61.130.155",
+    user: "admin_games",
+    password: "12345678",
+    database: "admin_games",
+  });
 
-    con.connect(function(err){//connectar-se(poder fer crides) a la base de dades
-        //Recollir si hi ha algun error
-        if(err) throw err;
-        var correo = req.query.correo;
-        var contrasenya = req.query.contrasenya;
-        con.query("SELECT * FROM usuari  where usuaricorreu='"+correo+"' and contrasenya='"+contrasenya+"'",function(err,result,fields){//fer consultes SELECT, WHERE, ORDER....
-            if(err) throw err;
-            //res.send(JSON.stringify(result))
-            res.header("Content-Type", "application/json");
-            if(result.length>0){
-                res.json(result);
-                console.log(result);
-            }else {
-                res.json({});
-            }
-        
-            
-            con.end();//Tancar la connexió, és molt important tancar-la sempre
-        });
-    });""
+  con.connect(function (err) {
+    //connectar-se(poder fer crides) a la base de dades
+    //Recollir si hi ha algun error
+    if (err) throw err;
+    var correo = req.query.correo;
+    var contrasenya = req.query.contrasenya;
+    con.query(
+      "SELECT * FROM usuari  where usuaricorreu='" +
+        correo +
+        "' and contrasenya='" +
+        contrasenya +
+        "'",
+      function (err, result, fields) {
+        //fer consultes SELECT, WHERE, ORDER....
+        if (err) throw err;
+        res.header("Content-Type", "application/json");
+        if (result.length > 0) {
+          res.json(result);
+          console.log(result);
+        } else {
+          res.json({});
+        }
+
+        con.end(); //Tancar la connexió, és molt important tancar-la sempre
+      }
+    );
+  });
 });
-/**------------------------------------------------------------------------------------------------------- */
-app.listen(3000,() =>{//port, no volem opcions de servidor
-    //no és obligatori posar-hi res
-    console.log("Carrot cake")
-}); 
+/*-------------------------------------------------------------------------------------------------------*/
+//WS per agafar els redactors de la web (Pàgina "about.html")
+app.get("/redactors", (req, res) => {
+  var con = mysql.createConnection({
+    host: "217.61.130.155",
+    user: "admin_games",
+    password: "12345678",
+    database: "admin_games",
+  });
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM redactors", function (err, result, fields) {
+      if (err) throw err;
+      res.header("Content-Type", "application/json");
+      res.json(result);
+      con.end();
+    });
+  });
+});
+/*-------------------------------------------------------------------------------------------------------*/
+//WS per agafar els últims articles aka "destacats" (Pàgina "index.html")
+app.get("/articles", (req, res) => {
+  var con = mysql.createConnection({
+    host: "217.61.130.155",
+    user: "admin_games",
+    password: "12345678",
+    database: "admin_games",
+  });
+  con.connect(function (err) {
+    if (err) throw err;
+    //Agafem els últims 3 articles ordenats per data
+    con.query("SELECT * FROM articles ORDER BY articledata LIMIT 3", function (
+      err,
+      result,
+      fields
+    ) {
+      if (err) throw err;
+      res.header("Content-Type", "application/json");
+      res.json(result);
+      con.end();
+    });
+  });
+});
+/*-------------------------------------------------------------------------------------------------------*/
+//WS per agafar tots articles (Pàgina "articles.html")
+app.get("/articles", (req, res) => {
+    var con = mysql.createConnection({
+      host: "217.61.130.155",
+      user: "admin_games",
+      password: "12345678",
+      database: "admin_games",
+    });
+    con.connect(function (err) {
+      if (err) throw err;
+      con.query("SELECT * FROM articles", function (
+        err,
+        result,
+        fields
+      ) {
+        if (err) throw err;
+        res.header("Content-Type", "application/json");
+        res.json(result);
+        con.end();
+      });
+    });
+  });
+  /*-------------------------------------------------------------------------------------------------------*/
+app.listen(3000, () => {
+  //port, no volem opcions de servidor
+  //no és obligatori posar-hi res
+  console.log("La connexió s'ha realitzat amb èxit");
+});
